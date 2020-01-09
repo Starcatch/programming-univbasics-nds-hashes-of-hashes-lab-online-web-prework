e657e370ac8fe9097ad08b550b90de90add811c7
Timur Mishiev <timurmishiev@gmail.com>
2020-01-09 22:47:27 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,31 +4,31 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+
+
+def naming_system
+  family = {
+    :label => "Family",
+    :sub_category => {
+    :label => "Genus",
+    :sub_category => {
+      :labl => "Species",
+    :sub_category => nil
+    }
+  }
+}
+
+
+
+order = {
+  :label => "Order",
+  :sub_category => family
+}
+
+domain = {
+  :lable => "Kingdom",
+}
+
 
 def naming_system
   # Remember:
